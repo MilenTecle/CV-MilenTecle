e790f24a078f84d0c3e068f65ad7aa2e61c43f1a
Milen Tecle <m_tecle@hotmail.com>
2023-12-08 14:37:35 +0000

    Render user information from Github

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,8 +2,26 @@ function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 3,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 60.1282,
+            lng: 18.6435
         }
     });
+
+
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const locations = [
+        { lat: 59.9793, lng: 18.8793 },
+        { lat: 59.3611, lng: 17.9711 },
+        { lat: 59.3344, lng: 18.0675 }
+    ];
+
+    const markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    const markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 }
