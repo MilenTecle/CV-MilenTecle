9ffe7d369b6a965a83bf2397902579365954de40
Milen Tecle <m_tecle@hotmail.com>
2023-12-08 20:37:25 +0000

    Implement EmailJS integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("service_5h7u1ni","template_b3t9q1g", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+
+    return false;
+}
